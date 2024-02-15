45881cb127196a9c0b0640b8350e554bb155cff4
bipproduction <bip.production.js@gmail.com>
2024-02-15 23:01:37 +0800

    auto push

--- index.js
+++ index.js
@@ -1,91 +1,28 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { Server, Socket } = require('socket.io');
-const cors = require('cors');
-require('colors')
 const yargs = require('yargs')
+const express = require('express')
+const http = require('http')
+const Websocket = require('ws')
 const path = require('path')
-const _ = require('lodash')
+require('colors')
 
-const app = express();
-// app.use(cors())
+const app = express()
+const server = http.createServer(app)
+const wss = new Websocket.Server({ server })
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "./public")))
-
-const server = createServer(app);
-
-/**
- * @type {Socket}
- */
-let sock;
-
-const io = new Server(server, {
-    allowEIO3: true,
-    cors: {
-        origin: "*",
-        credentials: true,
-        methods: ["GET", "POST"]
-    }
-});
-
-io.on('connection', (socket) => {
-    sock = socket
-
-    socket.on('server', (data) => {
-        console.log(data)
-    })
-
-    setTimeout(() => {
-        socket.emit("client", {
-            success: true,
-            evn: "client",
-            message: "from server"
-        })
-    }, 100);
-
-});
-
-
-app.get('/test', (req, res) => {
-    res.setHeader("Content-Type", "text/html")
-    res.sendFile(path.join(__dirname, "./public/test.html"))
-})
-
-app.post('/post/:evn?', (req, res) => {
-    const evn = req.params.evn
-    if (!evn) return res.json({
-        success: false,
-        message: "evn require"
-    })
-
-    if (sock) {
-        sock.emit(evn, {
-            success: true,
-            evn,
-            data: req.body
-        })
-    }
-
-    return res.json({
-        success: true,
-        evn,
-        data: req.body
-    })
-})
-
 
 yargs
     .command(
         "start",
         "start",
-        yargs => yargs
-            .options({
-                "port": {
-                    alias: "p",
-                    default: 3000
-                }
-            }),
+        yargs => yargs.options({
+            "port": {
+                alias: "p",
+                default: 3000
+            }
+        }),
         funStart
     )
     .demandCommand(1)
@@ -93,9 +30,22 @@ yargs
     .parse(process.argv.splice(2))
 
 
+wss.on('connection', (ws) => {
+
+    ws.on('message', function incoming(message) {
+        const data = JSON.parse(message)
+        wss.clients.forEach((client) => {
+            if ( client.readyState === Websocket.OPEN) {
+                client.send(JSON.stringify(data))
+            }
+        })
+    })
+
+})
+
+
 async function funStart(argv) {
     server.listen(argv.p, () => {
         console.log(`server running at http://localhost:${argv.p}`.green);
-    });
+    })
 }
-
---
+++ index2.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server, Socket } = require('socket.io');
+const cors = require('cors');
+require('colors')
+const yargs = require('yargs')
+const path = require('path')
+const _ = require('lodash')
+
+const app = express();
+// app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "./public")))
+
+const server = createServer(app);
+
+/**
+ * @type {Socket}
+ */
+let sock;
+
+const io = new Server(server, {
+    allowEIO3: true,
+    cors: {
+        origin: "*",
+        credentials: true,
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    sock = socket
+
+    socket.on('server', (data) => {
+        console.log(data)
+    })
+
+    setTimeout(() => {
+        socket.emit("client", {
+            success: true,
+            evn: "client",
+            message: "from server"
+        })
+    }, 100);
+
+});
+
+
+app.get('/test', (req, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.sendFile(path.join(__dirname, "./public/test.html"))
+})
+
+app.post('/post/:evn?', (req, res) => {
+    const evn = req.params.evn
+    if (!evn) return res.json({
+        success: false,
+        message: "evn require"
+    })
+
+    if (sock) {
+        sock.emit(evn, {
+            success: true,
+            evn,
+            data: req.body
+        })
+    }
+
+    return res.json({
+        success: true,
+        evn,
+        data: req.body
+    })
+})
+
+
+yargs
+    .command(
+        "start",
+        "start",
+        yargs => yargs
+            .options({
+                "port": {
+                    alias: "p",
+                    default: 3000
+                }
+            }),
+        funStart
+    )
+    .demandCommand(1)
+    .recommendCommands()
+    .parse(process.argv.splice(2))
+
+
+async function funStart(argv) {
+    server.listen(argv.p, () => {
+        console.log(`server running at http://localhost:${argv.p}`.green);
+    });
+}
+
