11e07c5443ba483a5ffc9c5e8068be72fe76b05d
bipproduction <bip.production.js@gmail.com>
2024-03-27 10:44:15 +0800

    auto push

--- src/apps/mqtt/mqtt.js
+++ src/apps/mqtt/mqtt.js
@@ -1,11 +1,27 @@
 const aedes = require('aedes')()
 const { createServer } = require('aedes-server-factory')
-const port = 8888
+const yargs = require('yargs')
 
 module.exports = async function mqtt() {
-    const httpServer = createServer(aedes, { ws: true })
-
-    httpServer.listen(port, function () {
-        console.log('websocket server listening on port ', port)
-    })
+    yargs
+        .command(
+            "start",
+            "start",
+            yargs => yargs
+                .options({
+                    port: {
+                        type: "number",
+                        default: 3003
+                    }
+                }),
+            async argv => {
+                const server = createServer(aedes)
+                server.listen(argv.port, () => {
+                    console.log(`MQTT server running on port ${argv.port}`)
+                })
+            }
+        )
+        .demandCommand(1)
+        .recommendCommands()
+        .parse(process.argv.slice(2))
 }
