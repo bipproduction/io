91aa366c4f98b855f06e3ff928dd479ee57749c9
bipproduction <bip.production.js@gmail.com>
2024-03-27 11:17:48 +0800

    auto push

--- src/apps/mqtt/mqtt.js
+++ src/apps/mqtt/mqtt.js
@@ -16,6 +16,22 @@ module.exports = async function mqtt() {
                 }),
             async argv => {
                 const server = createServer(aedes)
+
+                // Tambahkan header untuk mengizinkan CORS
+                server.on('request', (req, res) => {
+                    res.setHeader('Access-Control-Allow-Origin', '*')
+                    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+                    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+                    if (req.method === 'OPTIONS') {
+                        res.writeHead(200)
+                        res.end()
+                        return
+                    }
+
+                    aedes.handle(req, res)
+                })
+
                 server.listen(argv.port, () => {
                     console.log(`MQTT server running on port ${argv.port}`)
                 })
